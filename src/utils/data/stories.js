21143ab55490a0b4f88574a2517734c2b24c34b0
Vinícius Kanashiro <v.kanashiro9@gmail.com>
2024-06-20 20:34:38 -0300

    Update stories.js

--- src/utils/data/stories.js
+++ src/utils/data/stories.js
@@ -1,26 +1,26 @@
 export const STORIES = [
   {
     id: '1',
-    name: 'João Lemos',
-    image: 'https://files.oaiusercontent.com/file-MR78U8KNaWOTZJrEdn8hVUN6?se=2024-06-05T22%3A48%3A01Z&sp=r&sv=2023-11-03&sr=b&rscc=max-age%3D299%2C%20immutable&rscd=attachment%3B%20filename%3Dimage.png&sig=rStiTTgmCWZqcE%2BWl6LM8x%2BYt5dRnKLNSpHjH0Xxf1g%3D',
+    name: 'Rafael Kanashiro',
+    image: 'https://images.unsplash.com/photo-1712847333453-740d9665aa5d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8cGVvcGxlfGVufDB8fDB8fHww',
   },
   {
     id: '2',
-    name: 'Abigail Oliveira',
-    image: 'https://files.oaiusercontent.com/file-SxJsSzd8HcHbIkHFar8cABjS?se=2024-06-05T22%3A45%3A41Z&sp=r&sv=2023-11-03&sr=b&rscc=max-age%3D299%2C%20immutable&rscd=attachment%3B%20filename%3Dimage.png&sig=qUfsI%2BvG/m1wDzqup6V4obSmGd2fUwLgkV%2BJy%2Bo6roA%3D',
+    name: 'Marcia Oliveira',
+    image: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fHww',
   },
   {
     id: '3',
-    name: 'Júlia Campos',
-    image: 'https://files.oaiusercontent.com/file-6sgBVPuGKzRlRzBJdP8oC5AR?se=2024-06-05T22%3A47%3A20Z&sp=r&sv=2023-11-03&sr=b&rscc=max-age%3D299%2C%20immutable&rscd=attachment%3B%20filename%3Dimage.png&sig=EbVTGY8/1hcTCSIvYO%2BiRWniN91Ubzp5MCOvHXfQMe4%3D',
+    name: 'Rafaela Nordi',
+    image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww',
   },
   {
     id: '4',
-    name: 'Alex Nogueira',
-    image: 'https://files.oaiusercontent.com/file-KJIYVBAU5HgqggUq2E27zYYa?se=2024-06-05T22%3A46%3A46Z&sp=r&sv=2023-11-03&sr=b&rscc=max-age%3D299%2C%20immutable&rscd=attachment%3B%20filename%3Dimage.png&sig=fncQVGgxZszwqd5dUn%2BXrgM1VXNX4jMVI/4bSsto7O8%3D',
+    name: 'Gustavo Morais',
+    image: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fHww',
   }, {
     id: '5',
-    name: 'Maria Silva',
-    image: 'https://files.oaiusercontent.com/file-0NwpI8Pd9d3cj7yz7WDNQ37x?se=2024-06-05T22%3A48%3A28Z&sp=r&sv=2023-11-03&sr=b&rscc=max-age%3D299%2C%20immutable&rscd=attachment%3B%20filename%3Dimage.png&sig=qZttn2dJgFl6sOMcJu%2BB3jy5i6pQF7xyhRIioePIsiI%3D',
+    name: 'Tania Silvestre',
+    image: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGVvcGxlfGVufDB8fDB8fHww',
   },
 ];
